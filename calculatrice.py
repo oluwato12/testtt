abe8c5d32c7d6c226d4d8f75388c2d6edddb67bb
oluwato12 <enockrostand@gmail.com>
2025-10-06 13:27:33 +0200

    AI-test

---
+++ calculatrice.py
@@ -0,0 +1,12 @@
+nbr1 = int(input("Entrer le premier nombre "))
+nbr2 = int(input("Entrer le deuxieme nombre "))
+opera = input("Entre type d'operation que vous désirai éffectuer +,-,*,/ ")
+
+if (opera == "+"):
+    print(nbr1+nbr2)
+elif  (opera == "-"):
+    print(nbr1-nbr2)
+elif (opera == "*"):
+    print(nbr1*nbr2)
+elif (opera == "/"):
+    print(nbr1/nbr2)
