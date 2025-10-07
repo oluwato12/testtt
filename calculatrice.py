187d1477a06347cd8d6c894ed7e2e82efae815ef
oluwato12 <enockrostand@gmail.com>
2025-10-07 02:29:01 +0200

    up

--- calculatrice.py
+++ calculatrice.py
@@ -1,14 +1,35 @@
-nbr1 = int(input("Entrer le premier nombre "))
-nbr2 = int(input("Entrer le deuxieme nombre "))
-opera = input("Entre type d'operation que vous désirai éffectuer +,-,*,/ ")
+ #Pour les données de iris
+from sklearn.datasets import load_iris
 
-if (opera == "+"):
-    print(nbr1+nbr2)
-elif  (opera == "-"):
-    print(nbr1-nbr2)
-elif (opera == "*"):
-    print(nbr1*nbr2)
-elif (opera == "/"):
-    print(nbr1/nbr2)
+#Pour entrainer le modele et tester (train et test)
+from sklearn.model_selection import train_test_split
 
-#First deposit in git 
+#Pour l'algorithe d'apprentissage automatique (abre de decisison)
+from sklearn.tree import DecisionTreeClassifier
+
+#Pour le pourcenetage de bonne decision 
+from sklearn.metrics import accuracy_score
+
+#Avoir les donnees sous forme tableau 
+import pandas as pd
+iris = load_iris()
+#Donnees des fleur
+x = iris.data
+#Labels
+y = iris.target
+
+x_train, x_test, y_train ,y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+#Creaion le modele
+model = DecisionTreeClassifier()
+
+#Entrenement du modele
+model.fit(x_train,y_train)
+
+#Pour faire des predictions 
+y_pred = model.predict(x_test)
+
+#Verifier l'exatitude du modele 
+accuracy =  accuracy_score(y_test, y_pred)
+
+print("Taux de prédiction",accuracy)
